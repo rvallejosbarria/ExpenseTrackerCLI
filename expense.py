e8acb75c5f5d37bd44545478e2aa6ddbb082a618
rvallejosbarria <rvallejosbarria@gmail.com>
2024-08-25 19:29:24 -0400

    Define Expense class with init, str, to_dict, and from_dict methods

--- expense.py
+++ expense.py
@@ -1,9 +1,11 @@
 class Expense:
-  def __init__(self, expense_id, expense_date, description, amount, created_at, updated_at) -> None:
+  def __init__(self, expense_id, expense_date, description, category, amount, currency, created_at, updated_at) -> None:
     self.id = expense_id
     self.expense_date = expense_date
     self.description = description
+    self.category = category
     self.amount = amount
+    self.currency = currency
     self.created_at = created_at
     self.updated_at = updated_at
 
@@ -15,11 +17,13 @@ class Expense:
       "id": self.id,
       "expense_date": self.expense_date,
       "description": self.description,
+      "category": self.category,
       "amount": self.amount,
+      "currency": self.currency,
       "created_at": self.created_at,
       "updated_at": self.updated_at
     }
 
   @staticmethod
   def from_dict(data):
-    return Expense(data["id"], data["expense_date"], data["description"], data["amount"], data["created_at"], data["updated_at"])
+    return Expense(data["id"], data["expense_date"], data["description"], data["category"], data["amount"], data["currency"], data["created_at"], data["updated_at"])
